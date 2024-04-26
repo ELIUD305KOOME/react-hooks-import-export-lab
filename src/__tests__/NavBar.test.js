cc03889d46c6c354333021a07c488c3c730ef03b
ELIUDKOOME <eliudkoome305@gmail.com>
2024-04-26 15:40:22 +0300

    done

--- src/__tests__/NavBar.test.js
+++ src/__tests__/NavBar.test.js
@@ -1,7 +1,6 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import NavBar from "../components/NavBar";
-
 test("it is exported as a default export", () => {
   try {
     expect(() => render(<NavBar />)).not.toThrow();
@@ -9,3 +8,4 @@ test("it is exported as a default export", () => {
     throw new Error("Make sure to export this component!");
   }
 });
+
